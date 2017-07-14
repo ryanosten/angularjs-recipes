a633b7d09f30e8040cbd1c2cdd9784215a1bc888
Ryan Osten <ryan@Ryans-MBP.phub.net.cable.rogers.com>
2017-07-13 23:35:19 -0400

    initialized app

---
+++ public/scripts/app.js
@@ -0,0 +1,25 @@
+var app = angular.module('app', ['ngRoute'])
+
+  .controller('RecipesController', function($scope){
+    
+  })
+
+  .controller('categoriesController', function($scope, dataService){
+
+    dataService.getCategories(function(response){
+      console.log(response.data);
+      return response.data;
+    })
+  })
+
+  .service('dataService', function($http){
+
+    this.getRecipes = function(callback){
+      $http.get('/api/recipes')
+        .then(callback);
+      }
+    this.getCategories = function(callback){
+      $http.get('/api/categories')
+        .then(callback)
+      }
+  })
