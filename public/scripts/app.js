3664830552d135f410f6ab175b771e01c10287a1
Ryan Osten <ryan@Ryans-MBP.phub.net.cable.rogers.com>
2017-07-14 00:01:59 -0400

    added api calls to data service

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,14 +1,15 @@
 var app = angular.module('app', ['ngRoute'])
 
   .controller('RecipesController', function($scope){
-    
+
   })
 
   .controller('categoriesController', function($scope, dataService){
 
     dataService.getCategories(function(response){
       console.log(response.data);
-      return response.data;
+      let categoriesArray = response.data
+      return categoriesArray;
     })
   })
 
@@ -18,8 +19,40 @@ var app = angular.module('app', ['ngRoute'])
       $http.get('/api/recipes')
         .then(callback);
       }
+
     this.getCategories = function(callback){
       $http.get('/api/categories')
         .then(callback)
       }
+
+    this.foodItems = function(callback){
+      $http.get('/api/fooditems')
+        .then(callback)
+    }
+
+    this.getCategory = function(callback){
+      $http.get('/api/recipes?category={category}')
+        .then(callback)
+    }
+
+    this.getRecipe = function(callback){
+      $http.get('/api/recipes/{id}')
+        .then(callback)
+    }
+
+    this.updateRecipe = function(callback){
+      $http.put('/api/recipes/{id}')
+        .then(callback)
+    }
+
+    this.addRecipe = function(callback){
+      $http.post('/api/recipes')
+        .then(callback)
+    }
+
+    this.deleteRecipe = function(callback){
+      $http.delete('/api/recipes/{id}')
+        .then(callback)
+    }
+
   })
